0e74fb8158c041c0162c075275f86a5abd164b91
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-17 11:51:29 -0600

    pseudo code

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -1,3 +1,9 @@
 // z7RPLbJykv68026AD6lRTzuLiLERSaOX - API key for giphy usage
-// Global Variables
-let topic = [];
+// create buttons that display at top of screen
+// each button has a different search parameter
+// clicking on buttons pulls up, up to 10 images
+// clicking on an image will switch it to an animated gif
+// each image will display rating either below or above itself
+// include a search in column next to the display images/gifs
+// when search is processed button is added to top amongst all other buttons
+
