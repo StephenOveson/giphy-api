d34317196377b967d90f2a95968fe714d983ef03
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-17 20:48:40 -0600

    button builder in place

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -6,7 +6,32 @@
 // each image will display rating either below or above itself
 // include a search in column next to the display images/gifs
 // when search is processed button is added to top amongst all other buttons
+let gifTopic = [];
+let topButtons = $('#topic-buttons');
+let giphyDisplay = $('#giphy-display');
+let giphySearch = $('#giphy-search');
+let submit = $('#submit');
+let searchTerm;
+let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=z7RPLbJykv68026AD6lRTzuLiLERSaOX&limit=10";
 
-let topButtons = $('#topic-buttons')
-let giphyDisplay = $('#giphy-display')
-let giphySearch = $('#giphy-search')
+
+
+// Button Generator
+submit.on('click', function () {
+    event.preventDefault();
+    searchTerm = $('#new-giphy-search').val().trim();
+    gifTopic.push(searchTerm)
+    topButtons.empty();
+    if ($('#new-giphy-search').val() === '') {
+        return;
+    } else {
+        for (x = 0; x < gifTopic.length; x++) {
+            $('#new-giphy-search').val("");
+            let button = $('<button>');
+            button.addClass('btn btn-info')
+            button.attr('data-name', gifTopic[x])
+            button.text(gifTopic[x])
+            topButtons.append(button)
+        }
+    }
+})
