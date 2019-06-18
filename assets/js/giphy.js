9efe70803bea36074a73f5147bb37a2914b4f9bf
Spiritbr8ker <stephen.oveson@gmail.com>
2019-06-17 23:37:13 -0600

    giphy search using buttons and ajax being built issue capturing click apparently

--- assets/js/giphy.js
+++ assets/js/giphy.js
@@ -12,7 +12,7 @@ let giphyDisplay = $('#giphy-display');
 let giphySearch = $('#giphy-search');
 let submit = $('#submit');
 let searchTerm;
-let queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=z7RPLbJykv68026AD6lRTzuLiLERSaOX&limit=10";
+
 
 
 
@@ -28,10 +28,37 @@ submit.on('click', function () {
         for (x = 0; x < gifTopic.length; x++) {
             $('#new-giphy-search').val("");
             let button = $('<button>');
-            button.addClass('btn btn-info')
-            button.attr('data-name', gifTopic[x])
+            button.addClass('btn btn-info p-2 m-2 bd-highlight align-content-start flex-wrap')
+            button.attr('id', 'giphy-find')
+            button.attr('data-gif', gifTopic[x])
             button.text(gifTopic[x])
             topButtons.append(button)
         }
     }
+});
+
+// Giphy Search using the buttons created on click and labels
+let gifButtons = $('#giphy-find')
+gifButtons.on('click', function () {
+    let search = $(this).attr('data-gif').val().trim();
+    console.log(search);
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=z7RPLbJykv68026AD6lRTzuLiLERSaOX&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var gifStill = $("<img>");
+            gifStill.attr("src", results[i].images.fixed_height.url);
+            gifDiv.prepend(p);
+            gifDiv.prepend(gifStill);
+            $("#giphy-display").prepend(gifDiv);
+        }
+    })
+
 })
